a87f9512ea37f37870e0f7fb54d16628084f2b61
AbdMorheg <66905580+AbdMorheg@users.noreply.github.com>
2026-03-04 08:50:04 +0400

    Add files via upload
    updated

---
+++ js/chat.js
@@ -0,0 +1,10 @@
+
+window.$crisp = [];
+window.CRISP_WEBSITE_ID = "36577a61-8c42-4771-ab8b-c2f40e23b715";
+(function () {
+    d = document;
+    s = d.createElement("script");
+    s.src = "https://client.crisp.chat/l.js";
+    s.async = 1;
+    d.getElementsByTagName("head")[0].appendChild(s);
+})();
